a7bcefe306228cf1ab623e436b8f8d25d43603e2
xv1998 <1324841784@qq.com>
2022-05-19 23:11:16 +0800

    feat: 滑动窗口

--- projects/202205/159-至多包含两个不同字符的最长子串.js
+++ projects/202205/159-至多包含两个不同字符的最长子串.js
@@ -5,32 +5,33 @@
  * 输出: 5
  * 解释: t 是 "aabbb"，长度为5。
  */
-
+/**
+ * 思路：滑动窗口，用一个map去记录扫描的字母个数，right指针去增加，left指针去减少，当个数为1时就删除
+ * 1. 当右指针遍历到第三个不同字符时，停止向后查询，到左指针去一步步靠前，直到map里只有一个字符
+ */
 var lengthOfLongestSubstringTwoDistinct = function (s) {
   if (s.length < 2) return s.length;
   let left = 0;
   let right = 1;
   let max = 2;
   let map = new Map();
-  map.set(s[left], 0);
+  map.set(s[left], 1);
   while (right < s.length) {
-    if (map.get(s[right]) !== undefined || (map.size < 2 && s[left] !== s[right])) {
-      if (map.size < 2) {
-        map.set(s[right], right);
-      }
+    if (map.get(s[right]) || map.size < 2) {
+      map.set(s[right], (map.get(s[right]) || 0) + 1);
       right++;
     } else {
-      if (map.get(s[left + 1]) !== map.get(s[left])) {
+      if (max < right - left) {
+        max = right - left;
+      }
+      if (map.get(s[left]) > 1) {
+        map.set(s[left], map.get(s[left]) - 1);
+      } else {
         map.delete(s[left]);
       }
       left++;
-      console.log(left, right, map);
-    }
-    if (max < right - left) {
-      max = right - left;
     }
   }
-  console.log(left, right);
   if (max < right - left) {
     max = right - left;
   }
@@ -38,4 +39,4 @@ var lengthOfLongestSubstringTwoDistinct = function (s) {
   return max;
 };
 
-console.log(lengthOfLongestSubstringTwoDistinct("abccbbcccaaacaca"));
+console.log(lengthOfLongestSubstringTwoDistinct("ccaabbb"));
